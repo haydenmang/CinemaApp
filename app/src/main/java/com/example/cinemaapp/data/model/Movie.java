8f92eb37046cdd1d14ec62410b92326f6cf2aa12
haydenmang <vvuuvvaannngoc@gmail.com>
2026-05-16 11:02:17 +0700

    feat: Hoàn thiện luồng API Phim, Rạp, Suất chiếu

--- app/src/main/java/com/example/cinemaapp/data/model/Movie.java
+++ app/src/main/java/com/example/cinemaapp/data/model/Movie.java
@@ -3,12 +3,39 @@ package com.example.cinemaapp.data.model;
 import com.google.gson.annotations.SerializedName;
 
 public class Movie {
-    public int id;
-    public String title;
-    public String description;
-    public int duration;
+
+    @SerializedName("id")
+    private int id;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("description")
+    private String description;
+
+    @SerializedName("duration")
+    private int duration;
+
     @SerializedName("poster_url")
-    public String posterUrl;
+    private String posterUrl;
+
     @SerializedName("release_date")
-    public String releaseDate;
+    private String releaseDate;
+
+    //Getter để lấy dữ liệu
+    public int getId() {return id; }
+    public String getTitle() {return title; }
+    public String getDescription() {return description; }
+    public int getDuration() {return duration; }
+    public String getPosterUrl() {return posterUrl; }
+    public String getReleaseDate() {return releaseDate; }
+
+    //Setter để gán dữ liệu
+    public void setId(int id) {this.id = id; }
+    public void setTitle(String title) {this.title = title; }
+    public void setDescription(String description) {this.description = description;}
+    public void setDuration(int duration) {this.duration = duration;}
+    public void setPosterUrl(String posterUrl) {this.posterUrl = posterUrl;}
+    public void setReleaseDate(String releaseDate) {this.releaseDate = releaseDate;}
+
 }
