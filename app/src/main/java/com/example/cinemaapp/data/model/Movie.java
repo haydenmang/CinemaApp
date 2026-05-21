4d19f7021eed132f8a94c0ab4183cf7997d3bf9e
haydenmang <vvuuvvaannngoc@gmail.com>
2026-05-21 08:43:27 +0700

    feat: Sửa lại bảng Movies

--- app/src/main/java/com/example/cinemaapp/data/model/Movie.java
+++ app/src/main/java/com/example/cinemaapp/data/model/Movie.java
@@ -22,6 +22,24 @@ public class Movie {
     @SerializedName("release_date")
     private String releaseDate;
 
+    @SerializedName("age_limit")
+    private String ageLimit;
+
+    @SerializedName("trailer")
+    private String trailer;
+
+    @SerializedName("genre")
+    private String genre;
+
+    @SerializedName("director")
+    private String director;
+
+    @SerializedName("cast_list")
+    private String castList;
+
+    @SerializedName("rating")
+    private double rating;
+
     //Getter để lấy dữ liệu
     public int getId() {return id; }
     public String getTitle() {return title; }
@@ -29,6 +47,12 @@ public class Movie {
     public int getDuration() {return duration; }
     public String getPosterUrl() {return posterUrl; }
     public String getReleaseDate() {return releaseDate; }
+    public String getAgeLimit() {return ageLimit; }
+    public String getTrailer() {return trailer; }
+    public String getGenre() {return genre; }
+    public String getDirector() {return director; }
+    public String getCastList() {return castList; }
+    public double getRating() {return rating; }
 
     //Setter để gán dữ liệu
     public void setId(int id) {this.id = id; }
@@ -37,5 +61,11 @@ public class Movie {
     public void setDuration(int duration) {this.duration = duration;}
     public void setPosterUrl(String posterUrl) {this.posterUrl = posterUrl;}
     public void setReleaseDate(String releaseDate) {this.releaseDate = releaseDate;}
+    public void setAgeLimit(String ageLimit) {this.ageLimit = ageLimit; }
+    public void setTrailer(String trailer) {this.trailer = trailer; }
+    public void setGenre(String genre) {this.genre = genre; }
+    public void setDirector(String director) {this.director = director; }
+    public void setCastList(String castList) {this.castList = castList; }
+    public void setRating(double rating) {this.rating = rating; }
 
 }
